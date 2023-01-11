dfa5abf271befb518c8af4a3762bc9d00583e38b
Yiseull <omjl5123@catholic.ac.kr>
2023-01-11 20:17:46 +0900

    Second solution

--- 해시/포켓몬.py
+++ 해시/포켓몬.py
@@ -1,3 +1,9 @@
+# First Solution
 def solution(nums):
     answer, half = len(set(nums)), len(nums) // 2
     return half if answer > half else answer
+
+
+# Second Solution
+def solution(nums):
+    return min(len(nums) // 2, len(set(nums)))
