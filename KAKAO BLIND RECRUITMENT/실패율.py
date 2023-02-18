f3add0da7867f1f41ac4937af3f9f0864e0f21ec
Yiseull <omjl5123@catholic.ac.kr>
2023-02-19 00:07:39 +0900

    Second solution

--- KAKAO BLIND RECRUITMENT/실패율.py
+++ KAKAO BLIND RECRUITMENT/실패율.py
@@ -2,13 +2,13 @@ from collections import Counter
 
 
 def solution(N, stages):
-    stages.sort()
-    current = 0
-    counter = Counter(stages)
+    players = len(stages)
     fail = [[0, i + 1] for i in range(N)]
-    for i, stage in enumerate(stages):
-        if stage != current and stage <= N:
-            fail[stage - 1][0] = counter[stage] / len(stages[i:])
-            current = stage
+    counter = Counter(stages)
+    for i in range(1, N + 1):
+        if players:
+            clear = counter[i]
+            fail[i - 1][0] = clear / players
+            players -= clear
     fail.sort(key=lambda x: (-x[0], x[1]))
     return [i[1] for i in fail]
