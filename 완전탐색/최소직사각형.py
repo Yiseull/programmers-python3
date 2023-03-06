1c1c6ce548f4e290369c92d046d18be9d6c28ac0
Yiseull <omjl5123@catholic.ac.kr>
2023-03-06 19:56:33 +0900

    Second solution

--- 완전탐색/최소직사각형.py
+++ 완전탐색/최소직사각형.py
@@ -1,11 +1,2 @@
 def solution(sizes):
-    width, length = 0, 0
-    for size in sizes:
-        if size[0] > size[1]:
-            width = max(width, size[0])
-            length = max(length, size[1])
-        else:
-            width = max(width, size[1])
-            length = max(length, size[0])
-
-    return width * length
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
