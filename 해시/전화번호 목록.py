7c611c84612fbcb8753a7c12acfae8d7af298ee9
Yiseull <omjl5123@catholic.ac.kr>
2023-03-15 17:52:07 +0900

    Second solution

--- 해시/전화번호 목록.py
+++ 해시/전화번호 목록.py
@@ -1,8 +1,7 @@
 def solution(phone_book):
     phone_book.sort()
-    for i in range(1, len(phone_book)):
-        if phone_book[i - 1][0] == phone_book[i][0] and len(phone_book[i - 1]) <= len(phone_book[i]):
-            if phone_book[i - 1] == phone_book[i][:len(phone_book[i - 1])]:
-                return False
 
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
     return True
