2fe5d94f3e757b6959d819ca66977e04a2a8a0cf
Yiseull <omjl5123@catholic.ac.kr>
2023-04-14 21:01:03 +0900

    First solution

---
+++ 동적계획법(Dynamic Programming)/등굣길.py
@@ -0,0 +1,19 @@
+def solution(m, n, puddles):
+    dp = [[1] * m for _ in range(n)]
+
+    for x, y in puddles:
+        dp[y - 1][x - 1] = 0
+
+    for i in range(n):
+        for j in range(m):
+            if (i == 0 and j == 0) or dp[i][j] == 0:
+                continue
+
+            if i == 0:
+                dp[i][j] = dp[i][j - 1]
+            elif j == 0:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+
+    return dp[n - 1][m - 1]
