7636ac13726951851d0ad1b2692b36c1e4baca57
Yiseull <omjl5123@catholic.ac.kr>
2023-05-05 17:44:38 +0900

    :pencil2: 함수명 수정

--- 월간 코드 챌린지 시즌1/쿼드압축 후 개수 세기.py
+++ 월간 코드 챌린지 시즌1/쿼드압축 후 개수 세기.py
@@ -1,22 +1,22 @@
 def solution(arr):
     answer = [0, 0]
 
-    def quard_tree(x, y, n):
+    def quad_tree(x, y, n):
         if n == 1:
             return str(arr[x][y])
 
         half = n // 2
-        a = quard_tree(x, y, half)
-        b = quard_tree(x + half, y, half)
-        c = quard_tree(x, y + half, half)
-        d = quard_tree(x + half, y + half, half)
+        a = quad_tree(x, y, half)
+        b = quad_tree(x + half, y, half)
+        c = quad_tree(x, y + half, half)
+        d = quad_tree(x + half, y + half, half)
 
         if (a == '0' or a == '1') and a == b == c == d:
             return a
         else:
             return '(' + a + b + c + d + ')'
 
-    result = quard_tree(0, 0, len(arr))
+    result = quad_tree(0, 0, len(arr))
     answer[0] = result.count('0')
     answer[1] = result.count('1')
 
