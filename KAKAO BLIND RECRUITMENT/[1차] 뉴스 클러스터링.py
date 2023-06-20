d11dfa3389f6ed38055caa1a733a95ed16b20984
Yiseull <omjl5123@catholic.ac.kr>
2023-06-20 10:35:25 +0900

    :sparkles: First solution

---
+++ KAKAO BLIND RECRUITMENT/[1차] 뉴스 클러스터링.py
@@ -0,0 +1,55 @@
+from math import floor
+from collections import Counter
+
+from math import floor
+from collections import Counter
+
+
+def make_multiset(str) -> Counter:
+    multiset = Counter()
+    tmp = ''
+
+    for s in str:
+        if s.isalpha():
+            tmp += s.upper()
+            if len(tmp) == 2:
+                multiset[tmp] += 1
+                tmp = tmp[1]
+        else:
+            tmp = ''
+
+    return multiset
+
+
+def solution(str1, str2):
+    multiset1 = make_multiset(str1)
+    multiset2 = make_multiset(str2)
+
+    intersection_len, union_len = 0, 0
+    for s in set(multiset1) | set(multiset2):
+        intersection_len += min(multiset1[s], multiset2[s])
+        union_len += max(multiset1[s], multiset2[s])
+
+    if union_len == 0:
+        return 65536
+
+    return floor((intersection_len / union_len) * 65536)
+
+def solution(str1, str2):
+    multiset1 = make_multiset(str1)
+    multiset2 = make_multiset(str2)
+
+    intersection_len, union_len = 0, 0
+    for s in set(multiset1) | set(multiset2):
+        intersection_len += min(multiset1[s], multiset2[s])
+        union_len += max(multiset1[s], multiset2[s])
+
+    if union_len == 0:
+        return 65536
+
+    return floor((intersection_len / union_len) * 65536)
+
+
+
+solution(	"handshake", "shake hands")
+
